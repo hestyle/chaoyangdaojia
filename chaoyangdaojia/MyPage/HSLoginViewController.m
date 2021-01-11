bb4ccfd320e47d3c50381ba2edef8d9a8e172063
hestyle <hestyle@qq.com>
2021-01-11 16:16:15 +0800

    feat：重绘登录页面

--- chaoyangdaojia/MyPage/HSLoginViewController.m
+++ chaoyangdaojia/MyPage/HSLoginViewController.m
@@ -30,17 +30,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setEdgesForExtendedLayout:UIRectEdgeNone];
     [self.view setBackgroundColor:[UIColor whiteColor]];
     [self.navigationController setNavigationBarHidden:NO];
     [self setTitle:@"登录"];
     
+    [self initView];
+}
+
+
+#pragma mark - Private
+- (void)initView {
     self.logoImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo"]];
     [self.view addSubview:self.logoImageView];
     [self.logoImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_offset(180);
+        make.top.mas_offset(70);
         make.centerX.mas_equalTo(self.view);
-        make.width.mas_equalTo(80);
-        make.height.mas_equalTo(80);
+        make.width.mas_equalTo(100);
+        make.height.mas_equalTo(100);
     }];
     
     // 设置phoneNumber这一行
@@ -50,11 +57,11 @@
     [self.phoneNumberView.layer setBorderColor:[[UIColor blackColor] CGColor]];
     [self.view addSubview:self.phoneNumberView];
     [self.phoneNumberView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(self.logoImageView.mas_bottom).with.offset(20);
+        make.top.mas_equalTo(self.logoImageView.mas_bottom).mas_offset(60);
         make.centerX.mas_equalTo(self.view.mas_centerX);
-        make.left.mas_equalTo(self.view.mas_left).with.offset(40);
-        make.right.mas_equalTo(self.view.mas_right).with.offset(-40);
-        make.height.mas_equalTo(35);
+        make.left.mas_equalTo(self.view.mas_left).mas_offset(20);
+        make.right.mas_equalTo(self.view.mas_right).mas_offset(-20);
+        make.height.mas_equalTo(50);
     }];
     
     
@@ -63,19 +70,19 @@
     [self.phoneNumberView addSubview:self.phoneNumberLabel];
     [self.phoneNumberLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerY.equalTo(self.phoneNumberView.mas_centerY);
-        make.left.mas_equalTo(self.phoneNumberView.mas_left).with.offset(5);
+        make.left.mas_equalTo(self.phoneNumberView.mas_left).mas_offset(5);
         make.width.mas_equalTo(60);
-        make.height.mas_equalTo(30);
     }];
     
     self.phoneNumberTextField = [[UITextField alloc] init];
     [self.phoneNumberTextField setDelegate:self];
     [self.phoneNumberTextField setPlaceholder:@"请输入手机号"];
+    [self.phoneNumberTextField setKeyboardType:UIKeyboardTypeNumberPad];
     [self.phoneNumberView addSubview:self.phoneNumberTextField];
     [self.phoneNumberTextField mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerY.equalTo(self.phoneNumberView.mas_centerY);
-        make.left.mas_equalTo(self.phoneNumberLabel.mas_right).with.offset(10);
-        make.right.mas_equalTo(self.phoneNumberView.mas_right).with.offset(-2.5);
+        make.left.mas_equalTo(self.phoneNumberLabel.mas_right).mas_offset(10);
+        make.right.mas_equalTo(self.phoneNumberView.mas_right).mas_offset(-2.5);
         make.height.mas_equalTo(self.phoneNumberLabel.mas_height);
     }];
     
@@ -86,11 +93,11 @@
     [self.verifyCodeView.layer setBorderColor:[[UIColor blackColor] CGColor]];
     [self.view addSubview:self.verifyCodeView];
     [self.verifyCodeView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.mas_equalTo(self.phoneNumberView.mas_bottom).with.offset(20);
+        make.top.mas_equalTo(self.phoneNumberView.mas_bottom).mas_offset(10);
         make.centerX.mas_equalTo(self.view.mas_centerX);
-        make.left.mas_equalTo(self.view.mas_left).with.offset(40);
-        make.right.mas_equalTo(self.view.mas_right).with.offset(-40);
-        make.height.mas_equalTo(35);
+        make.left.mas_equalTo(self.view.mas_left).mas_offset(20);
+        make.right.mas_equalTo(self.view.mas_right).mas_offset(-20);
+        make.height.mas_equalTo(50);
     }];
     
     self.verifyCodeLabel = [[UILabel alloc] init];
@@ -100,7 +107,6 @@
         make.centerY.equalTo(self.verifyCodeView.mas_centerY);
         make.left.mas_equalTo(self.phoneNumberLabel.mas_left);
         make.width.mas_equalTo(self.phoneNumberLabel.mas_width);
-        make.height.mas_equalTo(self.phoneNumberLabel.mas_height);
     }];
     
     self.verifyCodeTextField = [[UITextField alloc] init];
@@ -109,13 +115,13 @@
     [self.view addSubview:self.verifyCodeTextField];
     [self.verifyCodeTextField mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerY.equalTo(self.verifyCodeView.mas_centerY);
-        make.left.mas_equalTo(self.verifyCodeLabel.mas_right).with.offset(10);
-        make.right.mas_equalTo(self.verifyCodeView.mas_right).with.offset(-110);
+        make.left.mas_equalTo(self.verifyCodeLabel.mas_right).mas_offset(10);
+        make.right.mas_equalTo(self.verifyCodeView.mas_right).mas_offset(-110);
         make.height.mas_equalTo(self.phoneNumberLabel.mas_height);
     }];
     
     self.getVerifyCodeButton = [UIButton new];
-    [self.getVerifyCodeButton setBackgroundColor:[UIColor systemBlueColor]];
+    [self.getVerifyCodeButton setBackgroundColor:[UIColor orangeColor]];
     [self.getVerifyCodeButton.layer setCornerRadius:5];
     [self.getVerifyCodeButton setTitle:@"获取验证码" forState:UIControlStateNormal];
     [self.getVerifyCodeButton setTitle:@"" forState:UIControlStateHighlighted];
@@ -123,38 +129,37 @@
     [self.view addSubview:self.getVerifyCodeButton];
     [self.getVerifyCodeButton mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerY.equalTo(self.verifyCodeView.mas_centerY);
-        make.left.mas_equalTo(self.verifyCodeTextField.mas_right).with.offset(5);
-        make.right.mas_equalTo(self.verifyCodeView.mas_right).with.offset(-5);
+        make.left.mas_equalTo(self.verifyCodeTextField.mas_right).mas_offset(5);
+        make.right.mas_equalTo(self.verifyCodeView.mas_right).mas_offset(-5);
         make.height.mas_equalTo(self.phoneNumberLabel.mas_height);
     }];
     
     self.loginButton = [UIButton new];
-    [self.loginButton setBackgroundColor:[UIColor systemBlueColor]];
+    [self.loginButton setBackgroundColor:[UIColor orangeColor]];
     [self.loginButton.layer setCornerRadius:5];
     [self.loginButton setTitle:@"登录" forState:UIControlStateNormal];
     [self.loginButton setTitle:@"" forState:UIControlStateHighlighted];
     [self.loginButton addTarget:self action:@selector(login) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:self.loginButton];
     [self.loginButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.verifyCodeLabel.mas_bottom).with.offset(10);
-        make.left.mas_equalTo(self.view.mas_left).with.offset(100);
-        make.right.mas_equalTo(self.view.mas_right).with.offset(-100);
+        make.top.equalTo(self.verifyCodeLabel.mas_bottom).mas_offset(20);
+        make.left.mas_equalTo(self.view.mas_left).mas_offset(100);
+        make.right.mas_equalTo(self.view.mas_right).mas_offset(-100);
         make.height.mas_equalTo(self.phoneNumberLabel.mas_height);
     }];
 }
-
-
-#pragma mark - Private
 /**
  *获取验证码
  */
-- (void)getVerifyCode{
+- (void)getVerifyCode {
     // 检查电话号码是否输入
     NSString *phoneNumberStr = [self.phoneNumberTextField text];
     if (phoneNumberStr == nil || phoneNumberStr.length == 0) {
-        [self.view makeToast:@"请输入电话号码！"];
+        [self.view makeToast:@"请输入电话号码！" duration:3.0f position:CSToastPositionCenter];
         return;
     }
+    [self.phoneNumberTextField endEditing:YES];
+    [self.verifyCodeTextField endEditing:YES];
     NSDictionary *paramters = @{@"phone":phoneNumberStr, @"type":@"login"};
     HSNetworkManager *manager = [HSNetworkManager shareManager];
     __weak __typeof__(self) weakSelf = self;
@@ -179,19 +184,21 @@
 /**
  *登录
  */
-- (void)login{
+- (void)login {
     // 检查电话号码是否输入
     NSString *phoneNumberStr = [self.phoneNumberTextField text];
     if (phoneNumberStr == nil || phoneNumberStr.length == 0) {
-        [self.view makeToast:@"请输入电话号码！"];
+        [self.view makeToast:@"请输入电话号码！" duration:3.0f position:CSToastPositionCenter];
         return;
     }
     // 检查验证码是否输入
     NSString *verifyCodeStr = [self.verifyCodeTextField text];
     if (verifyCodeStr == nil || verifyCodeStr.length == 0) {
-        [self.view makeToast:@"请输入验证码！"];
+        [self.view makeToast:@"请输入验证码！" duration:3.0f position:CSToastPositionCenter];
         return;
     }
+    [self.phoneNumberTextField endEditing:YES];
+    [self.verifyCodeTextField endEditing:YES];
     NSDictionary *data = @{@"phone":phoneNumberStr, @"type":@"login", @"username": phoneNumberStr, @"code": verifyCodeStr};
     NSDictionary *paramters = @{@"data":data};
     HSNetworkManager *manager = [HSNetworkManager shareManager];
@@ -208,7 +215,9 @@
             // 登录成功
             if ([responseDict[@"errcode"] isEqual:@(0)]) {
                 // 登录成功，返回前一个页面
-                [weakSelf.navigationController popViewControllerAnimated:YES];
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.5 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+                        [weakSelf.navigationController popViewControllerAnimated:YES];
+                });
             }
         });
         if ([responseDict[@"errcode"] isEqual:@(0)]) {
